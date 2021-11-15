98c8711a6c67854af55a150f0196f0ab08ac9e04
Occamies <Lasseknudsen01@hotmail.com>
2021-11-15 20:00:18 +0100

    upload 1

--- src/btree.c
+++ src/btree.c
@@ -9,6 +9,7 @@
 struct tree_node * Insert (int x, struct tree_node *t)
 {
   // Insert item x into the tree t
+  Insert (x,t);
 
   return NULL;
 }
@@ -16,6 +17,7 @@ struct tree_node * Insert (int x, struct tree_node *t)
 struct tree_node * Remove (int x, struct tree_node *t)
 {
  // Remove one item from the tree t
+ Remove (x,t);
 
   return NULL;
 }
@@ -25,6 +27,7 @@ int Contains (int x, struct tree_node *t)
 {
 
   // Return true if the tree t contains item x. Return false otherwise.
+  Contains (x,t);
 
   return NULL;
 
--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -8,7 +8,7 @@ extern "C"
 }
 
 // See Catch2's documentation: https://github.com/catchorg/Catch2/blob/devel/docs/tutorial.md#scaling-up
-
+/*
 TEST_CASE("linked_list")
 {
     	//exercise 2
@@ -19,7 +19,7 @@ TEST_CASE("linked_list")
 				   make_node (3,
 					      make_node (4,
 							  make_node (5,
-								    &SENTINEL_node)))));
+								    NULL)))));
 
 	sum = sum_squares (ns);	/* sum should equal 55 */
   	REQUIRE(sum==55);
@@ -27,7 +27,7 @@ TEST_CASE("linked_list")
 	sum=sum_squares(ns);
 	REQUIRE(sum==0);
 
-	ns = make_node (1,&SENTINEL_node);
+	ns = make_node (1,NULL);
 	sum=sum_squares(ns);
 	REQUIRE(sum==1);
 	
@@ -40,14 +40,14 @@ TEST_CASE("linked_list")
   	ns = make_node (1,
 			make_node (2,
 				   make_node (3,
-					      &SENTINEL_node)));
+					      NULL)));
 	//ns is 1->2->3
   	mns = map (ns, square);
 	//ns is 1->4->9
 	sum=sum_squares(ns);
 	//1+16+81 = 98
 	REQUIRE(sum==98);
-	free_list(ns);
+	free_list(mns);
 }
 
 TEST_CASE("btree")
